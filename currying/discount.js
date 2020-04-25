a21c73a17f989878156d014ee1e93e5f0eb4d7b3
Levi Donaldson <lcdbacanas@gmail.com>
2020-04-25 11:37:57 -0500

    Create discount.js

---
+++ currying/discount.js
@@ -0,0 +1,16 @@
+// Here we create a curry function for a discount that takes
+// just the percent but has access to the percent and price through closures
+
+const discount = percent => {
+    return price => { 
+        return price * percent; 
+    }
+}
+
+// now we can set various discounts from one function.
+const discount5 = discount(0.05);
+
+//when called this will give the 5% discount from whatever amount is passed.
+discount5(100); // = 5
+discount5(150); // = 7.5
+discount5(33); // = 1.65
